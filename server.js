e54dfbfc822c2b27139afda673abdd071b306204
Low Wei Shin <innovation4ezeem@gmail.com>
2026-05-28 14:37:11 +0800

    Revert "refactor: switch to custom server for enhanced error handling and update node version engine requirement"
    This reverts commit 2ab0cafa59108a5f6a1a8457f370cb32fd207c78.

--- server.js
+++ server.js
@@ -1,27 +1,22 @@
-process.chdir(__dirname);
 
-const { createServer } = require('http');
-const { parse } = require('url');
-const next = require('next');
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next')
 
-const dev = process.env.NODE_ENV !== 'production';
-const hostname = 'localhost';
-const port = process.env.PORT || 3000;
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
 
-const app = next({ dev, hostname, port });
-const handle = app.getRequestHandler();
+const port = process.env.PORT || 3000
 
 app.prepare().then(() => {
-  createServer(async (req, res) => {
-    try {
-      const parsedUrl = parse(req.url, true);
-      await handle(req, res, parsedUrl);
-    } catch (err) {
-      console.error('Error occurred handling', req.url, err);
-      res.statusCode = 500;
-      res.end('Internal Server Error');
-    }
-  }).listen(port, () => {
-    console.log(`> Ready on http://${hostname}:${port}`);
-  });
-});
+  createServer((req, res) => {
+    // Be sure to pass `true` as the second argument to `url.parse`.
+    // This tells it to parse the query portion of the URL.
+    const parsedUrl = parse(req.url, true)
+    handle(req, res, parsedUrl)
+  }).listen(port, (err) => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+})
